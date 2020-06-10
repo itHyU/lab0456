5fd7061e43fbc088226abb7ff4db950e989f8db5
Huy Tran <thedeath11b1@gmail.com>
2020-06-11 02:58:31 +0700

    test

--- BigSchool/Models/ApplicationUser.cs
+++ BigSchool/Models/ApplicationUser.cs
@@ -14,7 +14,7 @@ namespace BigSchool.Models
         [StringLength(255)]
         public string Name { get; set; }
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
+        {        
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
             // Add custom user claims here
